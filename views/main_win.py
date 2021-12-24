45a226052d9c54d9973e93d9c9d1984191516708
MasonChance <mason.fryberger@gmail.com>
2021-12-23 17:06:38 -0800

    outlined main_win.py with TODO's

---
+++ views/main_win.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+# Define and Configure main application window
+root = Tk()
+root.title('Equity 3D')
+root.configure(height=800, width=600)
+
+# TODO: Define Input box and its style,  place on root grid, 
+
+# TODO: Define Search Button Widget, Style, place on root grid
+
+# TODO: Define Clear Button Widget, Style, place on root grid
+
+# TODO: Define Config 3D Button Widget, Style, place on root grid
+
+# TODO: Define Help/Info Button Widget, Style, place on root grid
+
+# TODO: Define Info/tooltip Display frame Widget, style, place on root grid
+
+# TODO: Define Fundamental Data Label, style, place on root grid
+
+# TODO: Define Technical Data Label, style, place on root grid
+
+# TODO: Define Fundamental Data Frame, style, place on root grid
+
+# TODO: Define Technical Data Frame, style, place on root grid
+
+
+root.mainloop()
