50f98dc0d24a2f6159aecc371327aedbd99ab604
MasonChance <mason.fryberger@gmail.com>
2022-02-06 14:09:33 -0800

    search field, buttons and tooltip-display widgets; created and positioned

--- views/main_win.py
+++ views/main_win.py
@@ -1,29 +1,43 @@
 from tkinter import *
 
+
 # Define and Configure main application window
-root = Tk()
-root.title('Equity 3D')
-root.configure(height=800, width=600)
+main_win = Tk()
+main_win.title('Equity 3D')
+main_win.geometry('700x600')
 
-# TODO: Define Input box and its style,  place on root grid, 
+# Search Field Label and Entry; placement and config
+search_field = Entry(main_win, width=30)
+search_field.grid(row=0, column=0,padx=10, pady=15)
 
-# TODO: Define Search Button Widget, Style, place on root grid
+# MVP setup will require user to delete the default text manually. future updates will add an event listener to automatically select the whole field and highlight it so that the default text is replaced when the user begins typing in the field
 
-# TODO: Define Clear Button Widget, Style, place on root grid
+search_field.insert(0, "Enter Ticker or Company to Search")
+search_field.configure(font=('Comic Sans', 12, 'italic'), fg='#dbdbdb')
 
-# TODO: Define Config 3D Button Widget, Style, place on root grid
+# Search Button: placement, config, and handler/callback
+search_btn = Button(main_win, width=10, text='Search')
+search_btn.grid(row=0, column=1, padx=5, pady=15)
 
-# TODO: Define Help/Info Button Widget, Style, place on root grid
+# Clear Button: placement, config, and handler/callback
+clear_btn = Button(main_win, width=10, text='Clear')
+clear_btn.grid(row=0, column=2, padx=5, pady=15)
 
-# TODO: Define Info/tooltip Display frame Widget, style, place on root grid
+# Config 3D Button: placement, config and handler/callback
+# This should open a seperate window for selecting which elements to add to the 3D model and defining its axes.
+config_3d_btn = Button(main_win, width=10, text='Config 3D')
+config_3d_btn.grid(row=0, column=3, padx=5, pady=15)
 
-# TODO: Define Fundamental Data Label, style, place on root grid
+# Info and Tooltip Frame: placement, config, default values
+info_frame = Frame(main_win, width=700, height=200)
+info_frame.grid(row=1, columnspan=4, padx=5, pady=10)
 
-# TODO: Define Technical Data Label, style, place on root grid
+info_text = Text(info_frame, width=45, height=5, padx=5, bg='#dbdbdb', relief=SUNKEN)
+# TODO: create a "tooltip:Default" message and insert the variable here instead of hard coding. this should be done at the time of developing the tooltip utility. 
+info_text.insert(END, "Tooltips and Information will be displayed here")
+info_text.configure(font=('Comic Sans', 16, 'bold'), state='disabled')
+info_text.grid(row=0, column=0, columnspan=4)
 
-# TODO: Define Fundamental Data Frame, style, place on root grid
-
-# TODO: Define Technical Data Frame, style, place on root grid
 
 
-root.mainloop()
+main_win.mainloop()
